b132a081e2bd3b18ff0ef2fa8af52190be6620ca
Александр <62289323+busyhedg03@users.noreply.github.com>
2022-04-06 02:40:17 +0700

    Доделка

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,29 @@
 module Main where
-import Tictactoe
+import Tictactoe (startVsCPU, startPvP)
+import Control.Monad
+import System.IO ( hFlush, stdout )
+import Data.Time.Clock ( getCurrentTime, UTCTime(utctDayTime), DiffTime )
+import System.IO.Unsafe ( unsafePerformIO )
+import System.Random
+import System.Random (mkStdGen)
 
-main = start
+getTime :: IO Integer
+getTime = do
+    time <- getCurrentTime >>= return . utctDayTime
+    return $ diffTimeToSeconds time
+
+diffTimeToSeconds :: DiffTime -> Integer
+diffTimeToSeconds = floor . toRational
+main = main' [mkStdGen $ fromIntegral $ unsafePerformIO getTime]
+
+main' gen = do
+    putStr "Tic Tac Toe\n 1) vs CPU: cpu\n 2) 2 players: 2p\n exit\n> "
+    hFlush stdout
+    ans <- getLine
+    if ans == "1" || ans == "cpu" then do
+        startVsCPU gen
+        let (a,b) = randomR (0,9) (mkStdGen $ length gen) :: (Int, StdGen)
+        main' (gen ++ [b])
+    else when (ans == "2" || ans == "2p") $ do
+        startPvP
+        main' gen
--- src/Tictactoe.hs
+++ src/Tictactoe.hs
@@ -1,23 +1,26 @@
-module Tictactoe where
-import Data.Char(digitToInt)
-import Control.Monad.State
-import System.Random
-import Data.Time.Clock
+module Tictactoe (startVsCPU, startPvP) where
+import Control.Monad ( when )
+import Control.Monad.State ( MonadIO(liftIO), when )
+import System.Random ( mkStdGen, StdGen, Random(randomR) )
+import Data.Time.Clock ( getCurrentTime, UTCTime(utctDayTime), DiffTime )
+import System.IO.Unsafe ( unsafePerformIO )
+import Data.Functor ( (<&>) )
+import System.IO ( hFlush, stdout )
 data Player = X | O deriving(Eq)
 data Cell = Empty | Mark Player deriving(Eq)
 data Field = Field [Cell] deriving(Eq)
 data GameState = GameState Player Field deriving(Eq)
 
 instance Show Player where
-    show(X) = "X"
-    show(O) = "O"
+    show X = "X"
+    show O = "O"
 
 instance Show GameState where
-    show (GameState player field) = "Turn: " ++ show (player) ++ show (field)
+    show (GameState player field) = "Turn: " ++ show player ++ show field
 
 instance Show Cell where
-    show (Empty) = " "
-    show (Mark player) = show (player)
+    show Empty = " "
+    show (Mark player) = show player
 instance Show Field where
     show (Field list) = "\n " ++ show(list!!0) ++ " | " ++ show(list!!1) ++ " | " ++ show(list!!2) ++ "\n"
                         ++ replicate 11 '-' ++ "\n "
@@ -26,7 +29,7 @@ instance Show Field where
                         ++ show(list!!6) ++ " | " ++ show(list!!7) ++ " | " ++ show(list!!8)
 
 initGame :: GameState
-initGame = GameState X (Field (take 9 $ repeat Empty))
+initGame = GameState X (Field (replicate 9 Empty))
 
 nextPlayer :: Player -> Player
 nextPlayer X = O
@@ -40,9 +43,7 @@ letterToInt '1' = 1
 letterToInt '2' = 2
 letterToInt '3' = 3
 letterToInt _ = error "wrong coords"
---randomR (a,b)
-rand :: [Int] -> IO Int
-rand xs = getStdRandom (randomR (0, (length xs -1)))
+
 countCell :: Num a => [Char] -> a
 countCell str = letterToInt (str !! 0) * 3 + letterToInt (str !! 1) - 1
 
@@ -66,6 +67,7 @@ endGame (GameState pl (Field l))    | l!!0 == l!!1 && l!!1 == l!!2 && l!!0 /= Em
                                     | l!!1 == l!!4 && l!!4 == l!!7 && l!!1 /= Empty = True
                                     | l!!2 == l!!5 && l!!5 == l!!8 && l!!2 /= Empty = True
                                     | otherwise = Empty `notElem` l
+
 getPlayer :: GameState -> Player
 getPlayer (GameState pl _) = pl
 
@@ -76,26 +78,22 @@ gameLoopPvP game = do
         newt = makeTurn coord game
     if newt == game then putStrLn "You can't!" else putStrLn ""
     print newt
-    if endGame newt then putStrLn (player ++ " Win!") else gameLoopPvP newt
+    if endGame newt then do
+        putStr (player ++ " Win!")
+        putStr " Again? y/n\n> "
+        hFlush stdout
+        ans <- getLine
+        when (head ans == 'y') startPvP
+    else gameLoopPvP newt
 
-getRand xs = randomR (0, 8) (mkStdGen 1)
-
+makeTurnCPU :: (Ord p, Num p) => p -> GameState -> GameState
 makeTurnCPU ind (GameState player (Field field))  | cells /= field = GameState (nextPlayer player) (Field cells)
-                                                | otherwise = GameState player (Field field) where
+                                                  | otherwise = GameState player (Field field) where
     cells = changeField field ind player
+
 diffTimeToSeconds :: DiffTime -> Integer
 diffTimeToSeconds = floor . toRational
---test2 :: Integer
-test2 = do 
-    a <- liftIO test
-    return 1
 
-test :: IO Integer
-test = do
-    time <- getCurrentTime >>= return . utctDayTime
-    let a = diffTimeToSeconds time
-    return a
---getPOSIXTime
 genCPU :: GameState -> StdGen -> GameState
 genCPU gs generR = do
     let (ranNum, generNew) = randomR(0,8) generR :: (Int, StdGen)
@@ -103,7 +101,6 @@ genCPU gs generR = do
     if newt2 == gs then genCPU gs generNew
     else newt2
 
---getFromPlayer :: GameState -> IO GameState
 getFromPlayer :: GameState -> IO (GameState, String)
 getFromPlayer game = do
     coord <- getLine
@@ -114,23 +111,38 @@ getFromPlayer game = do
     else do
         return (newt, coord)
 
-gameLoop :: GameState -> IO ()
-gameLoop game = do
-    let player = show $ getPlayer game
-    (newt, coord) <- liftIO $ getFromPlayer game
-    let generR = mkStdGen $ countCell coord
-        newt = makeTurn coord game
-    print newt
-    if endGame newt then
-        putStrLn "You Win!" 
-    else do
-        let newt2 = genCPU newt generR
-        print newt2
-        gameLoop newt2
-    return()
 
-start :: IO ()
-start = do
+gameLoop :: GameState -> StdGen -> IO ()
+gameLoop game gen = do
+    if endGame game then do
+        print game
+        putStr "You Lose! Again? y/n\n> "
+        hFlush stdout
+    else do
+        let player = show $ getPlayer game
+        (newt, coord) <- liftIO $ getFromPlayer game
+        let newt = makeTurn coord game
+        if endGame newt then do
+            print newt
+            putStr "You Win! Again? y/n\n> "
+            hFlush stdout
+        else do
+            let newt2 = genCPU newt gen
+            print newt2
+            gameLoop newt2 gen
+
+startVsCPU :: [StdGen] -> IO ()
+startVsCPU gen = do
     let game = initGame
-    gameLoop game
-    return()
+    print game
+    gameLoop game $ last gen
+    let (a,b) = randomR (0,9) (mkStdGen $ length gen) :: (Int, StdGen)
+    let newGen = gen ++ [b]
+    ans <- getLine
+    when (head ans == 'y') $ startVsCPU newGen
+
+startPvP :: IO ()
+startPvP = do
+    let game = initGame
+    print game
+    gameLoopPvP game
